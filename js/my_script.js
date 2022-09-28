cbd0346abe4ddd85c76cb368b78ea2d70ae8b87d
Alejandro Jimenez Rios <aljr_2801@outlook.com>
2022-09-28 16:47:37 -0500

    Fixed the link issue in the figure camption in home using javascript

--- js/my_script.js
+++ js/my_script.js
@@ -1,11 +1,34 @@
-console.log("Hello World!");
+// Code to swap figure caption
+var screenWidth = window.screen.availWidth;
+if (screenWidth < 400) {
+    // Getting DOM elements
+    var ourFig = document.getElementById("home_about_figure");
+    var removeMe = document.getElementById("home_about_fig_cap");
+    // var ourPic = document.getElementById("home_about_pic")
 
-// var ourDiv = document.getElementById("our-div");
-// var newParagraph = document.createElement("p");
-// var copy = document.createTextNode("Hello, world!");
+    // Removing DOM elements
+    ourFig.removeChild(removeMe)
 
-// newParagraph.appendChild( copy );
-// ourDiv.appendChild( newParagraph );
+    // Creating DOM elements
+    var newFigCap = document.createElement("figcaption");
+    var newLink = document.createElement("a");
+    var newCap = document.createTextNode("Machu Pichu after attending ");
+
+    // Configuring new elements
+    newLink.target = "_blank";
+    newLink.id = "home_about_fig_cap_link";
+    newLink.href = "https://sahc2018.com/";
+    newLink.appendChild(document.createTextNode("SAHC Conference."));
+    newFigCap.id = "home_about_fig_cap";
+    newFigCap.appendChild(newCap);
+    newFigCap.appendChild(newLink);
+
+    // Appending DOM elements
+    ourFig.appendChild(newFigCap);
+
+    // Replacing caption elements
+    // ourFig.replaceChild(newFigCap, swapMe);
+} 
 
 
 
