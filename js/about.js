321979656d7bfaaadf1012492ee396db8019ffb2
Alejandro Jimenez Rios <alejand@oslomet.no>
2023-03-21 06:40:35 +0100

    Replaced images of special issues with slideshow in About page

--- js/about.js
+++ js/about.js
@@ -29,3 +29,20 @@ if (screenWidth < 400) {
     // Replacing caption elements
     // ourFig.replaceChild(newFigCap, swapMe);
 }
+
+
+// Code for slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
