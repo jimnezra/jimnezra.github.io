6adcd60aa05abb7a1e46526d4a85af1e4fbd1a4d
Alejandro Jimenez Rios <alejand@oslomet.no>
2023-03-31 10:13:46 +0200

    Removed slideshow

--- js/about.js
+++ js/about.js
@@ -31,18 +31,18 @@ if (screenWidth < 400) {
 }
 
 
-// Code for slideshow
-let slideIndex = 0;
-showSlides();
+// // Code for slideshow
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 5000); // Change image every 2 seconds
+// }
